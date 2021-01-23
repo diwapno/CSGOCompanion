13eb19ea48761441f2b4fceefaf6c29b690965b0
Ruben Magina <rubenmpvribeiro@gmail.com>
2021-01-23 18:19:24 +0000

    Star configuration

--- src/components/MissionItem.tsx
+++ src/components/MissionItem.tsx
@@ -13,61 +13,40 @@ const MissionItem: React.FC<MissionItemProps> = (props) => {
     return (
         <div>
             <IonItem lines="none">
-                {/* <IonLabel>
-                <h2>{props.title}</h2>
-                <p>{props.description}</p>
-            </IonLabel> */}
-                {/* <IonButton slot="start">
-                Start
-                </IonButton> */}
                 <IonLabel>
                     <h2>{props.title}</h2>
                     <p>{props.description}</p>
                 </IonLabel>
-                {/* <IonButton slot="end">
-                End
-          </IonButton> */}
-                {/* <IonRange min={0} max={3} step={1} snaps={true} color="secondary" /> */}
             </IonItem >
             <IonItem lines="none">
                 <IonGrid style={{ paddingLeft: 32 }}>
                     <IonRow>
-
-
                         <IonCol>
                             <div className="ion-text-end">
                                 {props.stars && props.stars === 3 &&
-                                    <IonIcon icon={star}></IonIcon>
+                                    <IonIcon className="star" icon={star}></IonIcon>
                                 }
                             </div>
                         </IonCol>
                         <IonCol>
                             <div className="ion-text-end">
                                 {props.stars && props.stars === 3 &&
-                                    <IonIcon icon={star}></IonIcon>
+                                    <IonIcon className="star" icon={star}></IonIcon>
                                 }
                             </div>
                         </IonCol>
                         <IonCol>
                             <div className="ion-text-end">
-                                <IonIcon icon={star}></IonIcon>
+                                <IonIcon className="star" icon={star}></IonIcon>
+                                {props.stars && props.stars === 1 &&
+                                    <IonIcon className="star" icon={star}></IonIcon>
+                                }
                             </div>
                         </IonCol>
                     </IonRow>
                 </IonGrid>
             </IonItem>
             <IonItem>
-                {/* <IonLabel>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-        </IonLabel> */}
-                {/* <IonButton slot="start">
-            Start
-            </IonButton> */}
-                {/* <p><IonLabel>Button Start/End</IonLabel></p> */}
-                {/* <IonButton slot="end">
-            End
-      </IonButton> */}
                 <IonRange min={0} max={props.stars} step={1} snaps={true} ticks={false} mode="md" />
             </IonItem >
         </div>
